71d592a356f663fbf338dab1ddfab6e9994f2069
chaca0309 <chaca0309@gmail.com>
2021-07-25 11:25:34 -0500

    Class 4

--- src/model/Formador.java
+++ src/model/Formador.java
@@ -1,23 +1,30 @@
 package model;
 
 public class Formador {
+    private String nombre;
+    private int codigo;
 
-private String nombre;
-private int codigo;
+    public Formador(String nombre, int codigo) {
+        super();
+        this.codigo = codigo;
+        this.nombre = nombre;
+    }
 
-public Formador(String nombre, int codigo) {
-    super();
-    this.nombre = nombre;
-    this.codigo = codigo;
-}
+    public Formador() {
+        super();
+        this.codigo = 0;
+        this.nombre = "Jon Doe";
+    }
 
-public String getNombre() {
-    return nombre;
-}
+    public int getCodigo() {
+        return codigo;
+    }
 
-public int getCodigo() {
-    return codigo;
-}
+    public String getNombre() {
+        return nombre;
+    }
 
-    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
--- src/view/Vista.java
+++ src/view/Vista.java
@@ -1,29 +1,24 @@
 package view;
 
-import model.Curso;
-import model.Formador;
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
-public class Vista {
+public class Vista extends JFrame{
+
+
+    private PanelOpciones panelOpc;
     
-    public static void main(String[] args) {
-        
-        Curso cursoFrances = new Curso(52441, "Frances 101", 'N', "Guillaume", 7400);
-        Curso cursoItaliano = new Curso(52442, "Italiano 102", 'T', "Vladimir Putin", 7401);
+    public Vista() {
+        super();
+        setSize(600, 400);
+        setTitle("Tripulantes");
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        setLayout(new BorderLayout());
 
-        Formador profesor = new Formador("Carlos", 321654);
-        System.out.println(profesor.getNombre());
-        System.out.println(profesor.getCodigo());
-        
-        System.out.println(cursoFrances.getFormador().getNombre());
-        System.out.println(cursoFrances.getFormador().getCodigo());
-        System.out.println(cursoItaliano.getFormador().getNombre());
-        System.out.println(cursoItaliano.getFormador().getCodigo());
+        panelOpc = new PanelOpciones();
 
-        //System.out.println(cursoFrances.getCodigo());
-        //System.out.println(cursoFrances.getNombre());
-        //System.out.println(cursoFrances.getJornada());
+        add(panelOpc, BorderLayout.SOUTH);
 
-        //System.out.println(cursoFrances.getFormador());
     }
-
+        
 }
